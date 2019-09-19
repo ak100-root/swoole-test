7c324c599c057d9ec8d40763ba368120ca936080
hulin.liu <root_shell@163.com>
2019-09-19 09:42:43 +0800

    xx

--- server.php
+++ server.php
@@ -1,12 +1,13 @@
 <?php
 $serv = new Swoole\Server('121.199.4.90', 9501);
+$serv->set(array('worker_num' => 4));
 $serv->on('connect', function ($serv, $fd){
     echo $fd."Client:Connect".PHP_EOL;
 });
 $serv->on('receive', function ($serv, $fd, $from_id, $data) {
     echo $data; //打印 接收到的数据
 
-    $serv->send($fd, "I am swoole reactor_num=".$serv->setting['reactor_num']); //发送字符串给客户端
+    $serv->send($fd, "I am swoole reactor_num=".$serv->setting['worker_num']); //发送字符串给客户端
 
     $serv->close($fd); // 注意：官方并不建议在这里关闭掉
 });
