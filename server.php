85f97ea593058d17d677161d234443f39698bd8d
hulin.liu <root_shell@163.com>
2019-09-19 09:55:37 +0800

    xx

--- server.php
+++ server.php
@@ -1,14 +1,25 @@
 <?php
-$serv = new Swoole\Server('121.199.4.90', 9501);
+/**
+ * test 1
+ * 对应的文档
+ * https://wiki.swoole.com/wiki/page/p-server.html
+ * https://wiki.swoole.com/wiki/page/142.html
+ *
+ * 客户端测试： 浏览器火狐 http://121.199.4.90:9501/
+ */
+$serv = new Swoole\Server('0.0.0.0', 9501);
 $serv->on('connect', function ($serv, $fd){
     echo $fd."Client:Connect".PHP_EOL;
 });
 $serv->on('receive', function ($serv, $fd, $from_id, $data) {
     echo $data; //打印 接收到的数据
 
-    $serv->send($fd, json_encode($serv->setting)); //发送字符串给客户端
+    $serv->send($fd, '默认设置：'.json_encode($serv->setting)); //发送字符串给客户端
 
     $serv->close($fd); // 注意：官方并不建议在这里关闭掉
 });
-
+$serv->on('close', function ($serv, $fd) {
+    echo "Client: Close.\n";
+});
 $serv->start();
+
