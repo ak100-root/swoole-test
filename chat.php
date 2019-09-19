e0540fa0ed1d7b168cb986968a796b226a668b70
hulin.liu <root_shell@163.com>
2019-09-19 13:45:17 +0800

    xx

--- chat.php
+++ chat.php
@@ -8,6 +8,7 @@
 $server = new swoole_websocket_server('0.0.0.0',9501);
 
 $server->on('message',function($server,$frame){
+    var_dump($frame->data);
     $con = $server->connection_list();
     foreach( $con as $fd){
         $server->push($fd,$frame->data);
