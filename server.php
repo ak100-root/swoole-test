d9ed5c106da0c8d40da574192b826b6512f7eac1
hulin.liu <root_shell@163.com>
2019-09-19 09:27:14 +0800

    xx

--- server.php
+++ server.php
@@ -1,12 +1,14 @@
 <?php
 $serv = new Swoole\Server('121.199.4.90', 9501);
 $serv->on('connect', function ($serv, $fd){
-    echo "Client:Connect".PHP_EOL;
+    echo $fd."Client:Connect".PHP_EOL;
 });
-$serv->on('receive', function ($serv, $fd, $reactor_id, $data) {
-    var_dump($data);
-    $serv->send($fd, 'Swoole: '.$data);
-    $serv->close($fd);
+$serv->on('receive', function ($serv, $fd, $from_id, $data) {
+    echo $data; //打印 接收到的数据
+
+    $serv->send($fd, "I am swoole"); //发送字符串给客户端
+
+    $serv->close($fd); // 注意：官方并不建议在这里关闭掉
 });
 $serv->on('close', function ($serv, $fd) {
     echo "Client $fd : Close".PHP_EOL;
