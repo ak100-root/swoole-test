3e681e8b85008fb7bcd2f8776ead2f1052bc7784
hulin.liu <root_shell@163.com>
2019-09-20 17:19:45 +0800

    xx

--- tcpServer.php
+++ tcpServer.php
@@ -5,8 +5,9 @@ $swoole->on('connect',function($ser,$fd){
     echo 'Client number '.$fd .' is clienting'.PHP_EOL;
 });
 
-$swoole->on('receive',function($ser,$fd,$from_id,$data){
+$swoole->on('receive',function($swoole,$fd,$reactor_id,$data){
         echo 'Client number '.$fd .'send info is ['.$data.'] '.PHP_EOL;
+        $swoole->send($fd,'Server has get your msg ['.$data.']');
 });
 
 $swoole->on('close',function($ser,$fd){
