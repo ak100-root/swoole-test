87bf43b9dfd98e9e8fddd2e44371b9424ac55916
hulin.liu <root_shell@163.com>
2019-09-20 17:14:30 +0800

    xx

--- tcpClient.php
+++ tcpClient.php
@@ -1,5 +1,5 @@
 <?php
-$client = new Swoole\Client(SWOOLE_SOCK_TCP);
+$client = new swoole_client(SWOOLE_SOCK_TCP);
 $conn = $client->connect('121.199.4.90',9501);
 if(!$conn){
     exit ('链接失败');
--- tcpServer.php
+++ tcpServer.php
@@ -1,12 +1,12 @@
 <?php
-$swoole = new Swoole\Server('0.0.0.0','9501');
+$swoole = new swoole_server('0.0.0.0','9501');
 
 $swoole->on('connect',function($ser,$fd){
     echo 'Client number '.$fd .' is clienting'.PHP_EOL;
 });
 
 $swoole->on('receive',function($ser,$fd,$from_id,$data){
-        echo 'Client number '.$fd .'send info is['.$data.']'.PHP_EOL;
+        echo 'Client number '.$fd .'send info is ['.$data.'] '.PHP_EOL;
 });
 
 $swoole->on('close',function($ser,$fd){
